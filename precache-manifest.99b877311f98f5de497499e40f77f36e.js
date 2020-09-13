cff654c50e0148e980787912190a7e4645814ff7
Aayush Mahapatra <dr.aaaaaaaa8@gmail.com>
2020-09-13 17:08:30 +0530

    Updates

---
+++ precache-manifest.99b877311f98f5de497499e40f77f36e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487a06c9a6171b8f2b0abea1b5d445ff",
+    "url": "/Dr-Apex/Blog-Fetch/index.html"
+  },
+  {
+    "revision": "babc36dd2911687718dc",
+    "url": "/Dr-Apex/Blog-Fetch/static/css/main.c6e5a0ff.chunk.css"
+  },
+  {
+    "revision": "217e53af5b9656a9cafc",
+    "url": "/Dr-Apex/Blog-Fetch/static/js/2.b55d45ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dr-Apex/Blog-Fetch/static/js/2.b55d45ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babc36dd2911687718dc",
+    "url": "/Dr-Apex/Blog-Fetch/static/js/main.054357a1.chunk.js"
+  },
+  {
+    "revision": "de1dd9440a4289fa819c",
+    "url": "/Dr-Apex/Blog-Fetch/static/js/runtime-main.f468aef7.js"
+  }
+]);
